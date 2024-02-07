36af872579e2aa1150a40d5946972b7f1321b309
Sainan123 <152434555+Sainan123@users.noreply.github.com>
2024-02-07 11:35:47 +0100

    Update flaskApp.py

--- flaskApp.py
+++ flaskApp.py
@@ -1,17 +1,24 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 import pickle
 import pandas as pd
 
-
 app = Flask(__name__)
 
-# Load the saved SVM model
-with open('best_svm_model.pkl', 'rb') as model_file:
-    loaded_svm_model = pickle.load(model_file)
+# Load the saved Gradient Regression model
+with open('Gradient_Regression.pkl', 'rb') as model_file:
+    loaded_Gradient_Regression_model = pickle.load(model_file)
+    
+with open('Random_forest.pkl', 'rb') as model_file:
+    Random_forest = pickle.load(model_file)
 
-# Load the saved Linear Regression model
-with open('linear_regressor_model.pkl', 'rb') as linear_model_file:
-    loaded_linear_regressor_model = joblib.load(linear_model_file)
+with open('best_svm_model.pkl', 'rb') as model_file:
+    best_svm_model = pickle.load(model_file)
+
+with open('linear_refression_model.pkl', 'rb') as model_file:
+    linear_refression_model = pickle.load(model_file)
+
+with open('best_decision_tree_model.pkl', 'rb') as model_file:
+    best_decision_tree_model  = pickle.load(model_file)    
 
 # Define the predict route
 @app.route('/predict', methods=['POST'])
